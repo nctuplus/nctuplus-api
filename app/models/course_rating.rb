ad916cf2c6f687d155d7aec0fadf59514d006fc5
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2018-11-12 05:31:36 -0800

    course_rating serializable_hash

--- app/models/course_rating.rb
+++ app/models/course_rating.rb
@@ -1,4 +1,15 @@
 class CourseRating < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  def serializable_hash(option = nil)
+	options = options.try(:dup) || {}
+	super({ **options }).tap do |result|
+	  result[:user_id] = user_id
+	  result[:course_id] = course_id
+	  result[:category] = category
+	  result[:score] = score
+	end
+  end
+  
 end
