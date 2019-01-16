bda0db2daec96cc1f76555751e14df44318ee441
Yu-Hao Chang <9203700@gmail.com>
2019-01-16 11:21:15 +0800

    Finish bulletin API

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ end
 100.times { FactoryBot.create :book } unless Book.all.size >= 1000
 100.times { FactoryBot.create :past_exam } unless PastExam.all.size >= 1000
 10.times { FactoryBot.create :event } unless Event.all.size >= 50
-10.times { FactoryBot.create :bulletin } unless Bulletin.all.size >= 50
+30.times { FactoryBot.create :bulletin } unless Bulletin.all.size >= 50
 
 FactoryBot.create :test_user unless User.find_by_name(:test).present?
 FactoryBot.create :admin_user unless User.find_by_name(:admin).present?
--- spec/factories/bulletins.rb
+++ spec/factories/bulletins.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :bulletin do
     title { Faker::Lorem.sentence }
-    content { Faker::Lorem.paragraph }
     category { Faker::Number.between(0, 1) }
     begin_time { Faker::Time.between(30.days.ago, Date.today, :all) }
     end_time { Faker::Time.between(Date.today, 30.days.since, :all) }
