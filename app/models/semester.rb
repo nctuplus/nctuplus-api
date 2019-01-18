80fa25af902c617eaba7c83d39e92aa7193acb19
Yu-Hao Chang <9203700@gmail.com>
2019-01-18 13:37:52 +0800

    Add serialization for past_exam

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -2,8 +2,12 @@ class Semester < ApplicationRecord
   has_many :courses
   def serializable_hash_for_course
     {}.tap do |result|
-        result[:year] = year 
-        result[:term] = term
+      result[:year] = year
+      result[:term] = term
     end
+  end
+
+  def serializable_hash_for_past_exam
+    serializable_hash_for_course
   end
 end
