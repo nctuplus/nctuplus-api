b8662d8dd13239bcc26e91ddb664a4e4ecffb2b5
Yu-Hao Chang <9203700@gmail.com>
2019-01-28 22:00:31 +0800

    Fix linter problem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     page = params[:page].try(:to_i) || 1
     per_page = params[:per_page].try(:to_i) || 15
-    filters = Comment.includes(:course, :user).ransack()
+    filters = Comment.includes(:course, :user).ransack
     @comments = filters.result(distinct: true).page(page).per(per_page)
 
     render json: {
@@ -14,9 +15,11 @@ class CommentsController < ApplicationController
       data: @comments
     }
   end
+
   def show
     render json: @comment
   end
+
   # POST /comments
   def create
     @comment = current_user.comments.build(comment_params)
@@ -27,17 +30,21 @@ class CommentsController < ApplicationController
       render json: @comment.errors, status: :unprocessable_entity
     end
   end
+
   # PATCH /comments/:id
   def update
     if @comment.user_id != current_user.id
-      render json: @comment.errors, status: :unauthorized
+      render json: { 'error': 'user does not match' }, status: :unauthorized
+    elsif @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
-    @comment.update_attributes(comment_params)
     @comment.course_ratings.each do |rating|
-        rating.score = param[:rating][rating.category].to_i
+      rating.score = param[:rating][rating.category].to_i
     end
-    render json: @comment
   end
+
   def destroy
     if @comment.user_id != current_user.id
       render json: @comment.errors, status: :unauthorized
@@ -45,10 +52,13 @@ class CommentsController < ApplicationController
       @comment.destroy
     end
   end
-private
+
+  private
+
   def set_comment
     @comment = Comment.includes(:course, :user, :course_ratings).find(params[:id])
   end
+
   def comment_params
     params.fetch(:comment, {}).permit(:title, :content, :anonymity, :course_id)
   end
--- app/models/course_rating.rb
+++ app/models/course_rating.rb
@@ -2,12 +2,11 @@ class CourseRating < ApplicationRecord
   belongs_to :user
   belongs_to :course
 
-  def serializable_hash(option = nil)
-	options = options.try(:dup) || {}
-	super({ **options }).tap do |result|
-	  result[:category] = category
-	  result[:score] = score
-	end
+  def serializable_hash(options = nil)
+    options = options.try(:dup) || {}
+    super({ **options }).tap do |result|
+      result[:category] = category
+      result[:score] = score
+    end
   end
-
 end
