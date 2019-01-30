369ac5a19f8b6dfeba0b98c69088c481caf41a05
Yu-Hao Chang <9203700@gmail.com>
2019-01-30 10:11:08 +0800

    Comments API

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :course
+  has_one :permanent_course, through: :course
+  has_many :teachers, through: :course
   belongs_to :user
   has_many :course_ratings, through: :user
+
+  # 重載course_ratings方法
+  # 使其只返回該筆心得所對應到的評分紀錄
+  def course_ratings
+    super.where(course_id: course_id, user_id: user_id)
+  end
 
   def serializable_hash(options = nil)
     options = options.try(:dup) || {}
@@ -13,5 +21,27 @@ class Comment < ApplicationRecord
         result[:rating][rating.category] = rating.score.to_s
       end
     end
+  end
+
+  # 建立該筆心得對應的評分紀錄
+  def create_course_ratings(ratings = [0, 0, 0])
+    ratings.each do |rating|
+      return false if rating > 5 || rating.negative?
+    end
+    ratings.each_with_index do |rating, index|
+      user.course_ratings.create course: course, category: index, score: rating
+    end
+    true
+  end
+
+  # 更新該筆心得對應的評分紀錄
+  def update_course_ratings(ratings = [0, 0, 0])
+    previous_rating = course_ratings.order(:category).pluck(:score)
+    return if previous_rating.eql?(ratings) || ratings.nil?
+
+    # Delete old ratings records
+    course_ratings.delete_all
+    # Create updated rating records
+    create_course_ratings(rating)
   end
 end
