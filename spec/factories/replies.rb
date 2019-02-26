661f678dc2ce7f83479fbcfb80f6ce21a43c75d6
Yu-Hao Chang <9203700@gmail.com>
2019-02-26 13:23:30 +0800

    Add reply factories

--- spec/factories/replies.rb
+++ spec/factories/replies.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :reply do
-    user_id { 1 }
-    content { "MyText" }
-    anonymity { false }
+    content { Faker::Lorem.paragraph }
+    anonymity { [true, false].sample }
+  end
+
+  factory :reply_for_rspec_test, parent: :reply do
+    user { FactoryBot.create :user }
+    comment { FactoryBot.create :comment_for_rspec_test }
   end
 end
