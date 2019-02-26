19609c31fbead5cf7465df891ed1c7e45400cd7b
Yu-Hao Chang <9203700@gmail.com>
2019-02-26 13:22:03 +0800

    Add model-level validations

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,6 @@
 class Reply < ApplicationRecord
   belongs_to :user
   belongs_to :comment
+
+  validates :user_id, :comment_id, :content, presence: { message: '%{attribute} can not be empty' }
 end
