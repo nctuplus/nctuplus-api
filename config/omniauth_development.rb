205620cdd46559c304b97a5edcb4fe6fecb6dc84
Yu-Hao Chang <9203700@gmail.com>
2019-05-14 00:49:35 +0800

    Adjust omniauth config

--- config/omniauth_development.rb
+++ config/omniauth_development.rb
@@ -2,12 +2,15 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :nctu,
            NCTU_OAUTH_CONFIG[:client_id],
            NCTU_OAUTH_CONFIG[:client_secret],
-           scope: 'profile'
+           scope: 'profile',
+           redirect_uri: 'https://{HOSTNAME}/auth/nctu/callback'
   provider :facebook,
            FACEBOOK_OAUTH_CONFIG[:client_id],
            FACEBOOK_OAUTH_CONFIG[:client_secret],
-           scope: 'email'
+           scope: 'email',
+           callback_url: 'https://{HOSTNAME}/auth/facebook/callback'
   provider :google_oauth2,
            GOOGLE_OAUTH_CONFIG[:client_id],
-           GOOGLE_OAUTH_CONFIG[:client_secret]
+           GOOGLE_OAUTH_CONFIG[:client_secret],
+           redirect_uri: 'https://{HOSTNAME}/auth/google_oauth2/callback'
 end
--- config/omniauth_production.rb
+++ config/omniauth_production.rb
@@ -2,12 +2,15 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :nctu,
            Rails.application.credentials[Rails.env.to_sym][:nctu][:client_id],
            Rails.application.credentials[Rails.env.to_sym][:nctu][:client_secret],
-           scope: 'profile'
+           scope: 'profile',
+           redirect_uri: 'https://plus.nctu.edu.tw/auth/nctu/callback'
   provider :facebook,
            Rails.application.credentials[Rails.env.to_sym][:facebook][:client_id],
            Rails.application.credentials[Rails.env.to_sym][:facebook][:client_secret],
-           scope: 'email'
+           scope: 'email',
+           callback_url: 'https://plus.nctu.edu.tw/auth/facebook/callback'
   provider :google_oauth2,
            Rails.application.credentials[Rails.env.to_sym][:google][:client_id],
            Rails.application.credentials[Rails.env.to_sym][:google][:client_secret]
+           redirect_uri: 'https://plus.nctu.edu.tw/auth/google_oauth2/callback'
 end
